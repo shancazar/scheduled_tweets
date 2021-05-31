5ab58c1aa8a606dfebb935c5f54383e98fe9bec2
shancazar <cianncazar.bootyard@gmail.com>
2021-05-30 20:16:17 -0700

    Additional features (sign up, sign in, log out, etc)

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
   #GET /about
   get "about-us", to: "about#index", as: :about
 
+  get "sign_up", to: "registrations#new"
+  post "sign_up", to: "registrations#create"
+
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create"
+
+  delete "logout", to: "sessions#destroy"
+
   root to: "main#index"
 end
